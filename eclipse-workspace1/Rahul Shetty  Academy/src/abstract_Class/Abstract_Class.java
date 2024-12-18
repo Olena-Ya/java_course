edd2c0724036a387d8683d07e57113de1743979f
Olena-Ya <yakovlio@herrenknecht.com>
2024-12-18 12:45:13 +0100

    1st commit

---
+++ eclipse-workspace1/Rahul Shetty  Academy/src/abstract_Class/Abstract_Class.java
@@ -0,0 +1,18 @@
+package abstract_Class;
+
+public abstract class Abstract_Class {
+
+	
+	public void AircraftSafetyGuidelines () {
+		System.out.println("Follow Safety Guidelines");
+		}
+	
+	public void EngineGuidelines () {
+	    System.out.println("Follow Engine Guidelines");
+		}
+	
+	public abstract void colorBody ();
+	
+}
+	
+
---
+++ eclipse-workspace1/Rahul Shetty  Academy/src/interfaces/CentralTraffic.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface CentralTraffic {
+	int a =4;
+	public void greenGo();
+	public void FlashingYellow();
+	public void redStop();
+	
+}
